fc6b864c7378b64ffff2f5323c046d2d71ecf4b6
Francisco Silveira <FranciscoSilveira@users.noreply.github.com>
2019-05-24 00:26:26 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -76,7 +76,7 @@ do
 			echo -e "\tPassed\n" >> $log_file
 		else
 			passed=$((passed+1)) 
-			echo -e "\tOutput was correct, but return code was not 0" >> $logfile
+			echo -e "\tOutput was correct, but return code was not 0\n" >> $logfile
 		fi
 	else
 		failed=$((failed+1))
